6582c6ce8269f4741fbde24950147a5ecbffba9e
mo-martin <momartin1@hotmail.co.uk>
2016-04-26 22:14:10 +0100

    Start

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$title = "";
+$desc = "";
+include("layout/header-home.php"); 
+?>
+
+<div class="home-work">
+	<div class="home-work-header"><h3><span>Some Of My Latest Work</span></h3></div>
+	<div class="home-work-container"><div class="home-work-container-snippet"><h4>Davisbrook Ltd</h4></div></div>
+	<div class="home-work-container"><div class="home-work-container-snippet"><h4>All About Me Keighley</h4></div></div>
+	<div class="home-work-container"><div class="home-work-container-snippet"><h4>DIP</h4></div></div>
+</div>
+	
+	
+<?php
+include("layout/footer.php"); 
+?>
