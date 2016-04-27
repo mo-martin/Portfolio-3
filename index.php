a10690c3a4b3ec940aa0ef101a4f7014e56e82db
mo-martin <momartin1@hotmail.co.uk>
2016-04-27 14:02:19 +0100

    Before Nav

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include("layout/header-home.php");
 	<div class="home-work-header"><h3><span>Some Of My Latest Work</span></h3></div>
 	<div class="home-work-container"><div class="home-work-container-snippet"><h4>Davisbrook Ltd</h4></div></div>
 	<div class="home-work-container"><div class="home-work-container-snippet"><h4>All About Me Keighley</h4></div></div>
-	<div class="home-work-container"><div class="home-work-container-snippet"><h4>DIP</h4></div></div>
+	<div class="home-work-container"><div class="home-work-container-snippet"><h4>Developing Infocomms</h4></div></div>
 </div>
 	
 	
